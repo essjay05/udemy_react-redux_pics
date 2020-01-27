1f2c0bd7771701061736f5ca16d3138a7b549da4
essjay05 <stepjoyous@gmail.com>
2020-01-26 22:48:03 -0800

    Trying to get access key to work to test api get request.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-    return (
-        <div className="ui container" style={{ marginTop: '1rem' }}>
-            <SearchBar />
-        </div>
-    )
-};
+class App extends React.Component {
+    
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`
+            }
+        });
+    }
+    render () {
+        return (
+            <div className="ui container" style={{ marginTop: '1rem' }}>
+                <SearchBar handleSubmit={this.onSearchSubmit}/>
+            </div>
+        )
+    } 
+}
 
 export default App;
