929f0c6551e5cf83528d6a75fb69e83177f7f7b5
essjay05 <stepjoyous@gmail.com>
2020-02-04 20:48:15 -0800

    Added api folder in src folder. Refactored App file.  Successfully tested as working still.

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    
-    onSearchSubmit(term) {
-        axios.get('https://api.unsplash.com/search/photos', {
-            params: { query: term },
-            headers: {
-                Authorization: `Client-ID ${process.env.REACT_APP_ACCESS_KEY}`
-            }
+    state = { images: [] };
+
+    onSearchSubmit = async (term) => {
+        const response = await unsplash.get('/search/photos', {
+            params: { query: term },    
         });
+        // console.log(response.data.results);
+        this.setState({ images: response.data.results });
+        // Promise syntax below
+            // .then((response) => {
+            //     console.log(response.data.results);
+            // });
     }
     render () {
         return (
             <div className="ui container" style={{ marginTop: '1rem' }}>
                 <SearchBar handleSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length} images
             </div>
         )
     } 
